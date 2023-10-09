acfae2162a9705c1b6c318a4fd3c515bae7d7b2a
ownssi <ownssi@gmail.com>
2023-10-09 00:51:33 -0700

    element implmented 1st trial

--- elements/pl-drag-drop/pl-drag-drop.js
+++ elements/pl-drag-drop/pl-drag-drop.js
@@ -1,9 +1,34 @@
+$(function() {
+    $("#data-frame tbody tr").draggable({
+        helper: "clone",
+        opacity: 0.5,
+        zIndex: 1000
+    });
 
-// fetch char_width_in_px
-let context = document.createElement("canvas").getContext("2d");
-context.font = "monospace";
-const char_width_in_px = context.measureText("0").width;
+    $("#data-frame th").draggable({
+        helper: "clone",
+        opacity: 0.5,
+        zIndex: 1000
+    });
 
+    // Make the table rows and columns droppable
+    $("#data-frame tbody tr, #data-frame th").droppable({
+        accept: "#data-frame tbody tr, #data-frame th",
+        drop: function(event, ui) {
+            var draggable = ui.draggable;
+            var droppable = $(this);
 
-  
-$(document).ready(ParsonsGlobal.setup);
+            // Swap the content of the dragged and dropped cells
+            var temp = droppable.html();
+            droppable.html(draggable.html());
+            draggable.html(temp);
+
+            const thElements = document.querySelectorAll('th');
+            const tdElements = document.querySelectorAll('td');
+
+        }
+    });
+    
+    //This code works when it's placed at the top
+    //$('#data-frame').append('<p>This text was added by a script.</p>');
+})
