9f73f708a3d3cd498e4b39e0e92190f74f7c11c6
Matthew Lee <matthewlee626@gmail.com>
2023-10-19 19:56:07 -0700

    problem changes

--- questions/pivotTables/server.py
+++ questions/pivotTables/server.py
@@ -1,7 +1,5 @@
-import random
 import pandas as pd
 import prairielearn as pl
-
 
 def generate(data):
     dummy_data = {
@@ -11,4 +9,15 @@ def generate(data):
         'Quantity': [100, 150, 200, 250]
     }
     input_df = pd.DataFrame(dummy_data)
+    pivoted_df = input_df.pivot(index='Category', columns='Item', values=['Value', 'Quantity'])
+
+    # Create lists of column and row labels and data values
+    columns = list(pivoted_df.columns)
+    index = list(pivoted_df.index)
+    values = pivoted_df.values.flatten().tolist()
+
     data["params"]["input_df"] = pl.to_json(input_df)
+    data["params"]["pivoted_df"] = pl.to_json(pivoted_df)
+    data["params"]["columns"] = columns
+    data["params"]["index"] = index
+    data["params"]["values"] = values
